821f656e1a0b012a220f72ad7892cfffb67f21e1
Chukwudi Festus <cfu.mdx@gmail.com>
2023-02-15 00:21:53 +0000

    Updating privacy py

--- Privacy.py
+++ Privacy.py
@@ -5,9 +5,10 @@ import time
 import spacy
 import datetime
 import re
-import en_core_web_sm
+import en_core_web_lg
 
-nlp = spacy.load("en_core_web_sm")
+
+nlp = spacy.load("en_core_web_lg")
 
 
 class Privacy(Scanner):
@@ -24,18 +25,18 @@ class Privacy(Scanner):
         return notification
 
     def __valueToCheckPii(self, value, nlp, key):
-        regex_email = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
+
+        regex_email = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
         regex_zip = r"\b\d{5}(?:-\d{4})?\b"
-        regex_phone = r"^(?!(\d{16}|\d{4}[\s-]\d{4}[\s-]\d{4}[\s-]\d{4}))(\+\d{1,2}[\s-]?)?(\(\d{3}\)|\d{3})[\s-]?(\d{3})[\s-]?(\d{4})(\s*|[\s-]\d{4})*$"
+        regex_phone = r"^(?:(?:\+44\s?\d{1,5}|\(?\d{5}\)?)\s?\d{3}\s?\d{3,4}|(?:\+1\s?)?\d{3}[\s.-]?\d{3}[\s.-]?\d{4}|\+39\s?(?:(?:\d{2}[\s.-]?)?\d{3}[\s.-]?\d{4}))$"
         regex_visacard = r"\b(4\d{3}[\s]\d{4}[\s]\d{4}[\s]\d{4}|4\d{3}[-]\d{4}[-]\d{4}[-]\d{4}|4\d{3}[.]\d{4}[.]\d{4}[.]\d{4}|4\d{3}\d{4}\d{4}\d{4})\b"
         regex_mastercard = r"\b(5[1-5][0-9]{2}[-\s]?[0-9]{4}[-\s]?[0-9]{4}[-\s]?[0-9]{4}|22[23][0-9]{12})\b"
         regex_socialmedia = r"(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)"
         regex_ips = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
         regex_postcode = r"\b([A-Z]{1,2}\d{1,2})\s*(\d[A-Z]{2})\b"
         regex_street_address = r"\b(?!\d{4}\b)(?!\d{5,}\b)(?!\d{4}\sby\sand\b)\d+\s+[A-Za-z]+\s+[A-Za-z]+\b"
-        regex_address = r"\b([\w\s]+),\s([\d\w\s]+),\s([A-Z]{2})\s(\d{5})\b"
-        regex_date = r"\b((\d{1,2}(st|nd|rd|th)\sday\s?of\s[A-Za-z]+\s?,?\s?\d{4})|(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{1,2},?\s?\d{4}|\d{1,2}/\d{1,2}/\d{4}|[A-Za-z]+\s\d{1,2}( \d{4})?)\b"
-        regex_dob = r"\b(born on|Date of birth)\b (\d{2}/\d{2}/\d{4}|\w+ \d{1,2}(st|nd|rd|th), \d{4})"
+        regex_date = r'(\d{4}-\d{2}-\d{2}|\d{3}T\d{2}:\d{2}:\d{2}|\d{2}\/\d{2}\/\d{4}|\d{2}-\d{2}-\d{4}|\d{2}\/\d{2}\/\d{2}|\d{2}-\d{2}-\d{2})'
+        regex_dob = r"(?:\b(?:(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2}(?:st|nd|rd|th)?(?:,\s+)?\d{4}))"
         regex_age = r"\b(\d{1,2}\syears|age\s?of\s?\d{1,2})\b"
 
         pii = {}
@@ -67,9 +68,6 @@ class Privacy(Scanner):
         find_street_address = re.findall(regex_street_address, value)
         if find_street_address:
             pii['STREETADDRESS'] = find_street_address
-        find_address = re.findall(regex_address, value)
-        if find_address:
-            pii['ADDRESS'] = find_address
         find_date = re.findall(regex_date, value)
         if find_date:
             pii['DATE'] = find_date
@@ -167,12 +165,12 @@ class Privacy(Scanner):
 
         current_date = datetime.datetime.now().date()
         return [{
-            "Job-type": "PRIVACY-VIOLATION",
-            "Modified-by": int(time.time()),
-            "Dataset ID": datasetID,
-            "Document ID": documentID,
-            "Status": "ALERT",
-            "SeverityScores": severityScores,
-            "Description": 'Personally identifiable information was detected in this document',
+            "job-type": "PRIVACY-VIOLATION",
+            "modified-by": int(time.time()),
+            "dataset": datasetID,
+            "document ID": documentID,
+            "status": "ALERT",
+            "severityScores": severityScores,
+            "description": 'Personally identifiable information was detected in this document',
             'Fields': items
         }]
