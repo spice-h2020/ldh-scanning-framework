9d43ef86b2e7ee05563bf60767e65c61d89acb62
Chukwudi Festus <cfu.mdx@gmail.com>
2023-02-28 06:57:40 +0000

    Updaing privacy module

--- Privacy.py
+++ Privacy.py
@@ -7,11 +7,17 @@ import datetime
 import re
 import en_core_web_sm
 
-
 nlp = spacy.load("en_core_web_sm")
 
 
 class Privacy(Scanner):
+    def __filter_english_words(self, text):
+        english_text = ''
+        for char in text:
+            if ord(char) < 128:
+                english_text += char
+        return english_text
+
     def __buildNotification(self, fieldName, fieldValue, piiType, piiValue, alertScore, ascoreName):
         notification = {
             "Field Name": fieldName,
@@ -31,10 +37,10 @@ class Privacy(Scanner):
         regex_phone = r"^(?:(?:\+44\s?\d{1,5}|\(?\d{5}\)?)\s?\d{3}\s?\d{3,4}|(?:\+1\s?)?\d{3}[\s.-]?\d{3}[\s.-]?\d{4}|\+39\s?(?:(?:\d{2}[\s.-]?)?\d{3}[\s.-]?\d{4}))$"
         regex_visacard = r"\b(4\d{3}[\s]\d{4}[\s]\d{4}[\s]\d{4}|4\d{3}[-]\d{4}[-]\d{4}[-]\d{4}|4\d{3}[.]\d{4}[.]\d{4}[.]\d{4}|4\d{3}\d{4}\d{4}\d{4})\b"
         regex_mastercard = r"\b(5[1-5][0-9]{2}[-\s]?[0-9]{4}[-\s]?[0-9]{4}[-\s]?[0-9]{4}|22[23][0-9]{12})\b"
-        #regex_socialmedia = r"(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)"
+        regex_socialmedia = r"(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)"
         regex_ips = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
         regex_postcode = r'[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][A-Z]{2}'
-        regex_street_address = r"\b(?!\d{4}\b)(?!\d{5,}\b)(?!\d{4}\sby\sand\b)\d+\s+[A-Za-z]+\s+[A-Za-z]+\b"
+        #regex_street_address = r"\b(?!\d{4}\b)(?!\d{5,}\b)(?!\d{4}\sby\sand\b)\d+\s+[A-Za-z]+\s+[A-Za-z]+\b"
         #regex_date = r'(\d{4}-\d{2}-\d{2}|\d{3}T\d{2}:\d{2}:\d{2}|\d{2}\/\d{2}\/\d{4}|\d{2}-\d{2}-\d{4}|\d{2}\/\d{2}\/\d{2}|\d{2}-\d{2}-\d{2})'
         regex_dob = r"(?:\b(?:(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2}(?:st|nd|rd|th)?(?:,\s+)?\d{4}))"
         regex_age = r"\b(\d{1,2}\syears|age\s?of\s?\d{1,2})\b"
@@ -56,18 +62,18 @@ class Privacy(Scanner):
         find_mastercard = re.findall(regex_mastercard, value)
         if find_mastercard:
             pii['MASTERCARD'] = find_mastercard
-        # find_socialmedia = re.findall(regex_socialmedia, value)
-        # if find_socialmedia:
-        #     pii['SOCIALMEDIA'] = find_socialmedia
+        find_socialmedia = re.findall(regex_socialmedia, value)
+        if find_socialmedia:
+            pii['SOCIALMEDIA'] = find_socialmedia
         find_ips = re.findall(regex_ips, value)
         if find_ips:
             pii['IPS'] = find_ips
         find_postcode = re.findall(regex_postcode, value)
         if find_postcode:
             pii['POSTCODE'] = find_postcode
-        find_street_address = re.findall(regex_street_address, value)
-        if find_street_address:
-            pii['STREETADDRESS'] = find_street_address
+        # find_street_address = re.findall(regex_street_address, value)
+        # if find_street_address:
+        #     pii['STREETADDRESS'] = find_street_address
         # find_date = re.findall(regex_date, value)
         # if find_date:
         #     pii['DATE'] = find_date
@@ -79,10 +85,10 @@ class Privacy(Scanner):
             pii['AGE'] = find_age
 
         # if len(pii) == 0:
-        # doc = nlp(value)
-        # for ent in doc.ents:
-        #     if ent.label_ in ["PERSON", "DATE", "ORG", "GPE", "LOC"]:
-        #         pii[ent.label_] = ent.text
+        #     doc = nlp(value)
+        #     for ent in doc.ents:
+        #         if ent.label_ in ["PERSON", "GPE", "LOC"]:
+        #             pii[ent.label_] = ent.text
         if len(pii) == 0:
             return {"code": 400}
         else:
@@ -109,7 +115,7 @@ class Privacy(Scanner):
             "VISACARD": 4,
             "PHONE": 4,
             "EMAIL": 4,
-            "STREETADDRESS": 3,
+            # "STREETADDRESS": 3,
             "STAE": 1,
             "COUNTRY": 1,
             # "DATE": 1,
@@ -118,24 +124,23 @@ class Privacy(Scanner):
             "IPS": 2,
             "AGE": 2,
             "MASTERCARD": 4,
-            # "SOCIALMEDIA": 3,
+            "SOCIALMEDIA": 3,
             'DATE OF BIRTH': 2,
 
         }
         # flatten the docObject
         flatObject = super().flattenObject(docObject)
-        # print('flatten object: ', flatObject)
-
         for key in flatObject:
-
-            # or re.search(r'_id', key):
-            if re.search(r'timestamp', key) or re.search(r'_ID', key) or re.search(r'JOB-TYPE', key):
+            if re.search(r'CREATEAT', key) or re.search(r'timestamp', key) or re.search(r'updated', key) or re.search(r"[#@$_]*[iI][dD]", key):
                 continue
             valueToCheckPii = flatObject[key]
-
             valueToCheckPii = self.__filter_english_words(str(valueToCheckPii))
             if (valueToCheckPii == ''):
                 continue
+
+            valueToCheckPii = re.sub(r"\b0\.\d+\b", "", valueToCheckPii)
+            valueToCheckPii = re.sub(
+                r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))", "", valueToCheckPii)
 
             values = self.__valueToCheckPii(str(valueToCheckPii), nlp, key)
             # print(key, values)
@@ -182,10 +187,3 @@ class Privacy(Scanner):
             "description": 'Personally identifiable information was detected in this document',
             'Fields': items
         }]
-
-    def __filter_english_words(self, text):
-        english_text = ''
-        for char in text:
-            if ord(char) < 128:
-                english_text += char
-        return english_text
